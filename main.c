a966828346e2b18491a8e40a9c418eabab19aaf9
Noktoborus <noktoborus@gmail.com>
2019-03-04 20:14:28 +0300

    remove frame saving logic

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* vim: ft=c ff=unix fenc=utf-8 ts=2 et
+/* vim: ft=c ff=unix fenc=utf-8 ts=2 sw=2 et
  * file: main.c
  */
 #include <stdio.h>
@@ -19,6 +19,8 @@
 
 #include <linux/videodev2.h>
 
+#include "circle_buffer.h"
+
 #define LOG_NOISY 0
 
 #ifndef MAX
@@ -29,7 +31,6 @@
 #endif
 
 #define WQUEUE_WRITE_BLOCK_SZ 4098
-#define BUFFERS_WRITE_COUNT 3
 #if 1
 # define BUFFERS_SWAP_COUNT 8
 #else
@@ -63,20 +64,10 @@
   } while (0)
 
 
-struct partinfo {
+struct wbuf {
   ev_io ev;
-  uint8_t *p;
-  /* descriptor of file to write */
   int fd;
-  /* maximum size of each stored frame */
-  size_t frame_max_size;
-  /* maximum count of stored frames */
-  size_t frame_capacity;
-  /* count and size of stored frames to buffer */
-  size_t frame_stored;
-  size_t frame_stored_size;
-  /* writed bytes to file */
-  size_t writed_to_disk;
+  struct circle_buffer cbf;
 };
 
 struct bufinfo {
@@ -95,24 +86,21 @@ struct devinfo {
   /* preseted values */
   char path[256];
 
-  /* approximately maximum size of compressed frame */
-  size_t frame_size_comp;
   /* size of uncompressed frame */
   size_t frame_size;
 
   size_t frame_width;
   size_t frame_height;
 
-  size_t frame_per_second;
   /* calculated values */
   /* input queue */
   struct bufinfo *queue;
   size_t queue_size;  
   size_t queued; /* count of queued buffers */
-  /* output queue */
-  struct partinfo *wqueue;
-  size_t wqueue_idx;
-  size_t wqueue_size;
+
+  /* output queue: frames and indexes */
+  struct wbuf wqueue_frame;
+  struct wbuf wqueue_index;
 
   /* counters */
   struct {
@@ -157,9 +145,7 @@ deinit_device(struct devinfo *dev)
     }
     free(dev->queue);
   }
-  if (dev->wqueue) {
-    free(dev->wqueue);
-  }
+  /* TODO: free circle_buffer */
 }
 
 void
@@ -204,45 +190,9 @@ init_device_rqueue_alloc(struct devinfo *dev)
 static bool
 init_device_wqueue_alloc(struct devinfo *dev)
 {
-  size_t capsize = 0u;
-  uint8_t *ptr = NULL;
-
-  capsize = dev->frame_size_comp * dev->frame_per_second;
-
-  dev->wqueue_size = BUFFERS_WRITE_COUNT;
-  dev->wqueue = calloc(dev->wqueue_size, sizeof(*dev->wqueue) + capsize);
-  if (!dev->wqueue) {
-    fprintf(stderr, "!  of memory while allocating write buffers: "
-                    "required %zu bytes\n",
-                    (capsize + sizeof(*dev->wqueue)) * dev->wqueue_size);
-    return false;
-  }
-
-  ptr = (void*)(dev->wqueue + dev->wqueue_size);
-
-  for (size_t i = 0u; i < dev->wqueue_size; i++) {
-    dev->wqueue[i].p = ptr + capsize * i;
-    dev->wqueue[i].frame_max_size = dev->frame_size_comp;
-    dev->wqueue[i].frame_capacity = dev->frame_per_second;
-    fprintf(stderr, "@ assign write buffer #%zu: %p, "
-                    "size: %zu, "
-                    "approx frame size: %zu, "
-                    "frame capcity: %zu\n",
-                    i,
-                    dev->wqueue[i].p,
-                    capsize,
-                    dev->wqueue[i].frame_max_size,
-                    dev->wqueue[i].frame_capacity);
-  }
-
-  fprintf(stderr, "@ summary write mem: %zu buffers, "
-                  "memory buffers: %zu bytes, "
-                  "memory queue: %zu bytes\n",
-                  dev->wqueue_size,
-                  capsize * dev->wqueue_size,
-                  sizeof(*dev->wqueue) * dev->wqueue_size);
-
-  return true;
+  /* TODO: allocate buffers */
+  fprintf(stderr, "! frame saving not supported\n");
+  return false;
 }
 
 /*
@@ -252,6 +202,7 @@ bool
 init_device_options(struct devinfo *dev)
 {
   struct v4l2_streamparm parm = {0};
+  size_t fps = 0u;
   parm.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
   if (!xioctl(dev->fd, VIDIOC_G_PARM, &parm)) {
     return false;
@@ -271,10 +222,10 @@ init_device_options(struct devinfo *dev)
       return false;
   }
 
-  dev->frame_per_second = (size_t)(parm.parm.capture.timeperframe.denominator /
-                                   parm.parm.capture.timeperframe.numerator);
+  fps = (size_t)(parm.parm.capture.timeperframe.denominator /
+                 parm.parm.capture.timeperframe.numerator);
 
-  fprintf(stderr, "@ frame per seconds: %zu\n", dev->frame_per_second);
+  fprintf(stderr, "@ frame per seconds: %zu\n", fps);
   return true;
 }
 
@@ -294,11 +245,9 @@ init_device(struct devinfo *dev)
 #if 1
   dev->frame_width = 1280;
   dev->frame_height = 720;
-  dev->frame_size_comp = 250000;
 #else
   dev->frame_width = 640;
   dev->frame_height = 480;
-  dev->frame_size_comp = 150000;
 #endif
   
   fprintf(stderr, "* open cam: %s\n", dev->path);
@@ -353,19 +302,6 @@ init_device(struct devinfo *dev)
   fprintf(stderr, "@  size : %"PRIu32"\n", fmt.fmt.pix.sizeimage);
   fprintf(stderr, "@  flags: 0x%08"PRIx32"\n", fmt.fmt.pix.flags);
   fprintf(stderr, "@  pixel format: '%.4s'\n", (char*)&fmt.fmt.pix.pixelformat);
-
-  /* driver can ignore setup frames */
-  if (fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG) {
-    fprintf(stderr,
-            "@ compressed frames: %zu bytes per frame\n",
-            dev->frame_size_comp);
-  } else {
-    fprintf(stderr,
-            "@ uncompressed frames: %zu bytes per frame\n",
-            dev->frame_size);
-
-    dev->frame_size_comp = dev->frame_size;
-  }
 
   if (!init_device_options(dev))
     return false;
@@ -436,112 +372,10 @@ capture(struct devinfo *dev)
   return true;
 }
 
-static struct partinfo *
-wqueue_get_free(struct devinfo *dev)
-{
-  static size_t fail_call_counter = 0u;
-  struct partinfo *wq = NULL;
-  size_t i = dev->wqueue_idx;
-
-  do {
-      wq = &dev->wqueue[i];
-      if (wq->frame_capacity <= wq->frame_stored)
-        continue;
-      /* update index */
-      dev->wqueue_idx = i;
-
-      if (fail_call_counter)
-        fprintf(stderr, "@ resume writing frames. lost count: %zu\n",
-                        fail_call_counter);
-
-      fail_call_counter = 0u;
-      /* return founded buffer */
-      return wq;
-  } while ((i = ((i + 1) % dev->wqueue_size)) != dev->wqueue_idx);
-
-  if (!fail_call_counter)
-    fprintf(stderr, "! no free write buffer. frame dropped\n");
-
-  fail_call_counter++;
-  return NULL;
-}
-
-static void
-wqueue_cb(struct ev_loop *loop, ev_io *w, int revents)
-{
-  struct partinfo *pinfo = (struct partinfo*)w;
-  size_t write_len = WQUEUE_WRITE_BLOCK_SZ;
-  int r = 0;
-
-  ev_io_stop(loop, &pinfo->ev);
-
-  if (pinfo->writed_to_disk + write_len > pinfo->frame_stored_size) {
-    write_len = pinfo->frame_stored_size - pinfo->writed_to_disk;
-  }
-
-  if (write_len == 0u) {
-    fprintf(stderr, "! write_len == 0. WTF!?\n");
-  } else {
-      r = write(pinfo->fd, pinfo->p + pinfo->writed_to_disk, write_len);
-  }
-
-  if (r == -1) {
-    fprintf(stderr, "! write(%zu) failed: %s\n", write_len, strerror(errno));
-  }
-
-  pinfo->writed_to_disk += r;
-
-  if (pinfo->writed_to_disk == pinfo->frame_stored_size) {
-    if (pinfo->frame_capacity == pinfo->frame_stored) {
-      close(pinfo->fd);
-      pinfo->fd = 0;
-      pinfo->frame_stored = 0;
-      pinfo->frame_stored_size = 0;
-      pinfo->writed_to_disk = 0;
-#if LOG_NOISY
-      fprintf(stderr, "@ free write buffer\n");
-#endif
-    }
-  } else {
-    ev_io_start(loop, &pinfo->ev);
-  }
-}
-
 void
 capture_process(struct ev_loop *loop, struct devinfo *dev, struct bufinfo *buf)
 {
-  struct partinfo *pinfo = wqueue_get_free(dev);
-
-  static unsigned long long frames_index = 0llu;
-
-  if (!pinfo)
-    return;
-
-  if (buf->filled > pinfo->frame_max_size) {
-    fprintf(stderr, "! frame to big: %zu > %zu. dropped\n",
-                    buf->filled,
-                    pinfo->frame_max_size);
-    /* TODO: copy black frame */
-  }
-  else {
-    memcpy(pinfo->p + pinfo->frame_stored_size, buf->p, buf->filled);
-    pinfo->frame_stored_size += buf->filled;
-    if (!pinfo->fd) {
-      char path[256] = {0};
-      snprintf(path, sizeof(path) - 1, "frames_%010llu.mjpeg", frames_index++);
-      pinfo->fd = open(path, O_CREAT | O_RDWR | O_NONBLOCK,
-                             S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP);
-      if (pinfo->fd == -1) {
-        fprintf(stderr, "! open(%s) failed. frame dropped: %s\n",
-                        path, strerror(errno));
-
-      }
-      /* start write event on part */
-      ev_io_init(&pinfo->ev, wqueue_cb, pinfo->fd, EV_WRITE);
-    }
-    ev_io_start(loop, &pinfo->ev);
-  }
-  pinfo->frame_stored++;
+  /* TODO: write frame to circle buffer */
 }
 
 static void
@@ -624,7 +458,8 @@ main(int argc, char *argv[])
 {
   struct ev_loop *loop = EV_DEFAULT;
   
-  init_device(&devinfo);
+  if (!init_device(&devinfo))
+    return EXIT_FAILURE;
 
   ev_io_init(&devinfo.ev, camera_cb, devinfo.fd, EV_READ);
   ev_io_start(loop, &devinfo.ev);
